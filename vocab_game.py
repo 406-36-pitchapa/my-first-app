052c778660170d42ef08c945c1764bf66233aff1
406-36-pitchapa <54739@yupparaj.ac.th>
2026-08-21 10:51:26 +0700

    vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -90,7 +90,7 @@ ans2 = st.text_input(
     value=st.session_state.ans2_val,
 )
 ans3 = st.text_input(
-    "ข้อ 3: The `c _ w` make milk. 🍑",
+    "ข้อ 3: The `c _ w` make milk. 🐄",
     value=st.session_state.ans3_val,
 )
 
