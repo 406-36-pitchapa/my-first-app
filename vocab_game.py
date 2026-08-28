5d37937839d0765fedda1939bca33a5aee136a60
406-36-pitchapa <54739@yupparaj.ac.th>
2026-08-28 10:24:55 +0700

    vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -114,7 +114,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2, ans3, ans4)
+    show_result_dialog(ans1, ans2, ans3)
 
 st.divider()
 st.write("นางสาวพิชชาภา ภิรมย์วงษ์ เลขที่ 36 ม.4/6")
